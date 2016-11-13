39d10c21b0c9727ccadf749b7b64052625e90974
gabriel <furstenheim@gmail.com>
2016-11-13 17:57:28 +0100

    Add cart

--- public/app-entry.js
+++ public/app-entry.js
@@ -6,7 +6,7 @@ require('./state-config')
 require('./main-component/main-controller')
 require('./menu-component/menu-controller')
 require('./recipe-component/recipe-controller')
-
+require('./cart-component/cart-controller')
 // services
 require('./services/recipe-service')
 require('./services/market-service')
